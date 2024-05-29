8ed829193607489803d4b6a36471978fb1e86c6b
Naman Pandey <2492004namanpandey@gmail.com>
2024-05-29 16:51:36 +0530

    error middleware typo fix

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -31,7 +31,7 @@ export const errorMiddleware = (err, req, res, next) => {
 
     const errorMessage = err.errors ? Object.values(err.errors).map((error) => error.message).join(" ") : err.message;
 
-    return res.status(err.statusCode).join({
+    return res.status(err.statusCode).json({
         success: false,
         message: errorMessage,
     })
