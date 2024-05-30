e7aed867b025bac77b1720d9732d68325b546a59
Naman Pandey <2492004namanpandey@gmail.com>
2024-05-30 19:34:24 +0530

    add delete message route

--- backend/routers/messageRoute.js
+++ backend/routers/messageRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { deleteMessage, sendAllMessage, sendMessage } from "../controllers/messageController.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/send", sendMessage);
 router.get("/getAll", sendAllMessage);
-router.delete("/delete", deleteMessage);
+router.delete("/delete/:id", isAuthenticated, deleteMessage);
 
 export default router;
