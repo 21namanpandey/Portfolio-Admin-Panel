466c4f9c17f24209a1fde58fc92073411c42d6cd
Naman Pandey <2492004namanpandey@gmail.com>
2024-06-05 22:33:29 +0530

    fix css in AddTimeline component

--- dashbaord/src/pages/sub-components/AddTimeline.jsx
+++ dashbaord/src/pages/sub-components/AddTimeline.jsx
@@ -50,7 +50,7 @@ const AddTimeline = () => {
             <div className="flex justify-center items-center min-h-[100vh] sm:gap-4 sm:py-4 sm:pl-14">
                 <form className="w-[100%] px-5 md:w-[650px] " onSubmit={handleNewTimeline}>
                     <div className="space-y-12">
-                        <div className="border-b border-gray-900 pb-12">
+                        <div className="border-b border-gray-900/10 pb-12">
                             <h2 className="font-semibold leading-7 text-gray-900 text-3xl text-center">
                                 ADD A NEW TIMELINE
                             </h2>
