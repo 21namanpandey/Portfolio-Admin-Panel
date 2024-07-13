48d161adbf302fa7fef14e7351da0d51a4d2d6c6
Naman Pandey <2492004namanpandey@gmail.com>
2024-07-13 12:26:43 +0530

    update footer section

--- portfolio/src/pages/sub-components/Footer.jsx
+++ portfolio/src/pages/sub-components/Footer.jsx
@@ -7,6 +7,9 @@ const Footer = () => {
             <h1 className="text-tubeLight-effect text-3xl mt-5 flex text-center justify-center   tracking-[8px] ">
                 Thanks For Scrolling
             </h1>
+            <p className="text-center mt-4 text-sm">
+                    © {new Date().getFullYear()} | Made with ❤️ Naman Pandey
+                </p>
         </footer>
     );
 };
