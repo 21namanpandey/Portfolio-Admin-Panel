b447139ad537b093ad69a998c62f3ad2e94a79e0
Naman Pandey <2492004namanpandey@gmail.com>
2024-07-17 09:22:43 +0530

    update text styling

--- portfolio/src/pages/sub-components/Footer.jsx
+++ portfolio/src/pages/sub-components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="p-5 mt-6 w-full max-w-[1050px] mx-auto">
             <hr />
             <h1 className="text-tubeLight-effect text-3xl mt-5 flex text-center justify-center   tracking-[8px] ">
-                Thanks For Scrolling
+                Thanks For Visiting
             </h1>
             <p className="text-center mt-4 text-sm">
                     © {new Date().getFullYear()} | Made with ❤️ Naman Pandey
--- portfolio/src/pages/sub-components/MyApps.jsx
+++ portfolio/src/pages/sub-components/MyApps.jsx
@@ -18,9 +18,17 @@ const MyApps = () => {
     return (
         <div>
             <div className="w-full flex flex-col gap-8 sm:gap-12  ">
-                <h1 className=" text-[2rem] sm:text-[2.75rem] md:text-[3rem] lg:text-[3.8rem] tracking-[15px]  text-tubeLight-effect dancing_text mx-auto w-fit ">
-                    MY APPS
-                </h1>
+                <div className="relative mb-12">
+                    <h1
+                        className="hidden sm:flex gap-4 items-center text-[2rem] sm:text-[2.75rem] md:text-[3rem] lg:text-[3.8rem] leading-[56px] md:leading-[67px] lg:leading-[90px] tracking-[15px] mx-auto w-fit font-extrabold  "
+                        style={{ background: "hsl(222.2 84% 4.9%)" }}
+                    >
+                        MY
+                        <span className="text-tubeLight-effect font-extrabold"> APPS</span>
+                    </h1>
+
+                    <span className="absolute w-full h-1 top-7 sm:top-7 md:top-8 lg:top-11 z-[-1] bg-slate-200 "></span>
+                </div>
                 <div className=" grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4  ">
                     {apps &&
                         apps.map((element) => {
