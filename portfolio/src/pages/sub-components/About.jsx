52b4b7835c1c68ff24c7e54b947b7e97e47e99e2
Naman Pandey <2492004namanpandey@gmail.com>
2024-07-18 11:07:57 +0530

    fix heading styling issue

--- portfolio/src/pages/sub-components/About.jsx
+++ portfolio/src/pages/sub-components/About.jsx
@@ -21,15 +21,25 @@ const About = () => {
 
     return (
         <div className="w-full flex flex-col overflow-x-hidden">
-            <div className="relative">
+            <div className="relative mb-12">
                 <h1
-                    className="flex gap-4 items-center text-[2rem] sm:text-[2.75rem] md:text-[3rem] lg:text-[3.8rem] leading-[56px] md:leading-[67px] lg:leading-[90px] tracking-[15px] mx-auto w-fit font-extrabold  "
+                    className="hidden sm:flex gap-4 items-center text-[2rem] sm:text-[2.75rem] md:text-[3rem] lg:text-[3.8rem] leading-[56px] md:leading-[67px] lg:leading-[90px] tracking-[15px] mx-auto w-fit font-extrabold  "
                     style={{ background: "hsl(222.2 84% 4.9%)" }}
                 >
                     ABOUT
-                    <span className="text-tubeLight-effect font-extrabold">ME</span>
+                    <span className="text-tubeLight-effect font-extrabold">
+                        ME
+                    </span>
                 </h1>
-                <span className="absolute w-full h-1 top-7 sm:top-7 md:top-8 lg:top-11 z-[-1] bg-slate-200"></span>
+                <h1
+                    className="flex sm:hidden gap-4 items-center text-[2rem] sm:text-[2.75rem] md:text-[3rem] lg:text-[3.8rem] leading-[56px] md:leading-[67px] lg:leading-[90px] mx-auto w-fit font-extrabold"
+                    style={{ background: "hsl(222.2 84% 4.9%)" }}
+                >
+                    <span className="tracking-[5px]">ABOUT</span>
+                    <span className="text-tubeLight-effect font-extrabold tracking-[5px]">ME</span>
+                </h1>
+
+                <span className="absolute w-full h-1 top-7 sm:top-7 md:top-8 lg:top-11 z-[-1] bg-slate-200 "></span>
             </div>
             <div className="grid md:grid-cols-2 my-8 sm:my-20 gap-14">
                 <div className="flex justify-center items-center">
